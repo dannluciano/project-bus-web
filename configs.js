feef3db6a00d00ac68916885d74bcd10c6de2ec7
Dann Luciano <dannluciano@gmail.com>
2023-10-21 20:52:49 -0300

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,9 +1,8 @@
 export default {
-    HOSTNAME: '127.0.0.1',
-    PORT: 3000,
-    USERNAME: process.env['USERNAME'] || 'admin',
-    PASSWORD: process.env['PASSWORD'] || 'ifpiifpi',
-    DATABASE: 'database.sqlite3',
-    SCHEMA: 'schema.sql'
-}
-
+  HOSTNAME: "127.0.0.1",
+  PORT: 3000,
+  USERNAME: process.env["USERNAME"] || "admin",
+  PASSWORD: process.env["PASSWORD"] || "ifpiifpi",
+  DATABASE: process.env["DATABASE"] || "database.sqlite3",
+  SCHEMA: process.env["SCHEMA"] || "schema.sql",
+};
